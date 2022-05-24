dafbf68473c314e87e6c8cd7e3f09b0b55cd3eb0
Mohalk <Mohalk@DESKTOP-IHOPFCS>
2022-05-24 12:04:51 -0300

    CM - Server imoboliaria

---
+++ src/test/java/br/com/saks/serverimobiliaria/ServerImobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saks.serverimobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerImobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
